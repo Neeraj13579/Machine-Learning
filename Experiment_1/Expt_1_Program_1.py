aaba4c6fc0672bf055496e9c27d25bc88bdba6e0
Neeraj Kumar <117432902+Neeraj13579@users.noreply.github.com>
2024-10-04 15:11:58 +0530

    Add files via upload

--- Experiment_1/Expt_1_Program_1.py
+++ Experiment_1/Expt_1_Program_1.py
@@ -1,1 +1,8 @@
-
+numbers =[1,2,3,4,5,6,7]
+result = []
+for i in numbers:
+   result.append(i*i)
+print("Name : Neeraj Kumar")
+print("UIN : 211P065 ")
+print("Square of the given numbers are: ")
+print(result)
